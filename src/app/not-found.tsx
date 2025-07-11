afd041aa445bad11bf95826cb9af9255021fb85f
WILSONPLUR <yourshows12@gmail.com>
2025-07-11 14:29:29 +0300

    feat: made 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import styles from "./not-found.module.css";
 import { WatchBg } from "../../public/not-found";
+import { Button } from "./components/Button/Button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ChatButton } from "./components/Chat/components/ChatButton/ChatButton";
+import { MainContext } from "./context";
+import { ChatMenu } from "./components/Chat/ChatMenu";
 
 const NotFound = () => {
+  const { back } = useRouter();
+  const { setMenuOpened, menuOpened } = useContext(MainContext);
+  const openMenu = () => {
+    setMenuOpened(true);
+  };
+  const goBack = () => {
+    back();
+  };
   return (
     <section className={styles.notFound}>
       <div className={styles.notFoundContainer}>
@@ -12,8 +27,24 @@ const NotFound = () => {
             <p className={styles.notFoundDescription}>
               Сторінку, яку ви шукаєте, не знайдено. Можливо, вона вже зникла,
               змінила адресу або ніколи не існувала.
-            </p>
+            </p>{" "}
+            <Button
+              variant="solid"
+              bgColor="#fff"
+              color="#000"
+              classNames={`${styles.notFoundBtn} ${styles.notFoundBtnBack}`}
+              onClick={goBack}
+            >
+              Назад
+            </Button>
+            <Button
+              variant="text"
+              classNames={`${styles.notFoundBtn} ${styles.notFoundBtnChat}`}
+            >
+              <Link href="/chat"> Відкрити чат з AI агентом</Link>
+            </Button>
           </div>
+
           <img
             src={WatchBg.src}
             alt="watch background"
@@ -21,6 +52,8 @@ const NotFound = () => {
           />
         </div>
       </div>
+      {menuOpened ? null : <ChatButton onClick={openMenu} />}
+      {menuOpened && <ChatMenu />}
     </section>
   );
 };
