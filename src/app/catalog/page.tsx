63b011d3328b5d3e4ac1c1c3bfe0fd42f96fd863
WILSONPLUR <yourshows12@gmail.com>
2025-07-12 00:08:06 +0300

    feat: basic structure

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styles from "./page.module.css";
 
 const Catalog = () => {
-  return <div>page</div>;
+  return (
+    <section className={styles.catalog}>
+      <div className={styles.catalogContainer}>
+        <div className={styles.catalogContent}>
+          <div className={styles.catalogText}>
+            <h1>Каталог</h1>
+            <p>
+              Інтернет-магазин годинників, де стиль зустрічається з точністю. Ми
+              пропонуємо широкий вибір чоловічих, жіночих та дитячих годинників{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Catalog;
